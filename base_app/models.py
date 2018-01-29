e9fec5b6e46a5cd702d5fe7a36bd9ca4d2139aca
Marcel Fox <contato@marcelfox.com>
2018-01-29 08:50:44 -0300

    Improving passwords adding more security

--- base_app/models.py
+++ base_app/models.py
@@ -1,13 +1,17 @@
 from base_app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Profile(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
     firstName = db.Column(db.String(100), nullable=False)
     surName = db.Column(db.String(100), nullable=False)
     description = db.Column (db.Text, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-
+    
 
 # Create Tables into our database:
 db.create_all()
@@ -15,7 +19,8 @@ db.create_all()
 
 # Add some information:
 DummyData = Profile(
-    username='tester',
+    username = 'Tester',
+    password = generate_password_hash('1q2w3e4r'),
     firstName='Test',
     surName='User',
     description='I am a dummy data on your database',
@@ -27,3 +32,12 @@ DummyData = Profile(
 if Profile.query.filter_by(email='test@uwsgi.dom').first() is None:
     db.session.add(DummyData)
     db.session.commit()
+
+# Query a specific username from 'Profile' table:
+q_var = Profile.query.filter_by(username='Tester').first()
+
+# Now we can check 'username', 'password' and so forth:
+# q_var.username, q_var.password ...
+#
+# For instance, you can check if the password is correct by using:
+# check_password_hash(q_var.username, '1q2w3e4r')
