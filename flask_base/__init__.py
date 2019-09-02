d0acd8ea527cfd64595fcab8f343d135a8c30669
Marcel Fox <marcelfox@live.com>
2019-09-02 01:43:18 -0300

    Taking all flask_base files

--- flask_base/__init__.py
+++ flask_base/__init__.py
@@ -1,30 +1,20 @@
-import os
 
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_base import models, views
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    app.config.from_pyfile('config.py')
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    db = SQLAlchemy(app)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
-# a simple page that says hello
+    # a simple page that says hello
     @app.route('/')
     def hello():
         return 'Hello, World!'
