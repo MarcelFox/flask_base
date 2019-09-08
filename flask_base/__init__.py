0af5cb1aa35b8fb7ac63888560932d3f8d7f63b5
Marcel Fox <marcelfox@live.com>
2019-09-07 23:47:46 -0300

    adjusting uwsgi and config files

--- flask_base/__init__.py
+++ flask_base/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 
-def create_app():
+def create_app(config):
     from . import views, models
 
     app = Flask(__name__)
-    app.config.from_object('config')
+    app.config.from_object(config)
     models.init_app(app)
     views.init_app(app)
 
