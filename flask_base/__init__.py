ad7e9cfbebd77b5e82640bc2a921d832011fb303
Marcel Fox <marcelfox@live.com>
2019-09-07 23:55:07 -0300

    returning uwsgi old config

--- flask_base/__init__.py
+++ flask_base/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 
-def create_app(config):
+def create_app():
     from . import views, models
 
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_object('config')
     models.init_app(app)
     views.init_app(app)
 
