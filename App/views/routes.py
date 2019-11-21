0fd22f218cad2223e7a86a242054cd502c66253e
MarcelFox <marcelfox@live.com>
2019-11-21 12:44:41 -0300

    adjusting routes.js

--- App/views/routes.py
+++ App/views/routes.py
@@ -8,18 +8,19 @@ index = Blueprint('index', __name__)
 
 @index.route('/')
 def main_page():
-    admin = User(username='admin',
-                 email='admin@example.com',
-                 password='coconuts')
 
-    app.logger.info("[ADMIN]: " + admin.username)
-
-    user = User.query.filter_by(username='admin').first()
-
-    if user is None:
+    def createUser():
+        admin = User(username='admin',
+                    email='admin@example.com',
+                    password='coconuts')
+        # app.logger.info("[ADMIN]: " + admin.username)
         db.session.add(admin)
         db.session.commit()
 
+    user = User.query.filter_by(username='admin').first()
+    if user is None:
+        createUser()
+        
         
     return render_template('index.html', user=user)
 
