548cc9926f509494ad7f00d81cb461bcffb1cb6d
MarcelFox <marcelfox@live.com>
2019-11-24 19:55:48 -0300

    BlackJack Ajax completed

--- App/static/js/script.js
+++ App/static/js/script.js
@@ -3,17 +3,6 @@ $(document).ready(function () {
 
         let playerOldValue = $('#hit').text()
 
-        if ($('#tbCard1').text() == '?') {
-            $.get('/gethit', e => {
-                $("#tbCard1").html(e.name)
-                    (e.value)
-            })
-
-        } else {
-            let tbOldValue = $('#game').text()
-            $.get('/gethit', e => { $("#game").html(tbOldValue + e.name) })
-        }
-
         $.get('/getCardValue', { 'value': $('#hit').text().slice(-2, -1) }).done(e => {
             scndLastValue = parseInt(e)
             $.get('/getCardValue', { 'value': $('#hit').text().slice(-1) }).done(e => {
@@ -24,10 +13,10 @@ $(document).ready(function () {
 
                     let value = (parseInt(plScore) + parseInt(e))
                     if (value > 21) {
-                        $("#plStatus").html('YOU LOSE: '+value)
+                        $("#plStatus").html('YOU LOSE!')
                         $("#addHit").prop("disabled", true);
-                    } else if (value == 21){
-                        $("#plStatus").html('YOU WIN: '+value)
+                    } else if (value == 21) {
+                        $("#plStatus").html('YOU WIN!')
                         $("#addHit").prop("disabled", true);
                     }
                     $("#plScore").html(value)
@@ -41,15 +30,81 @@ $(document).ready(function () {
             method: 'GET',
             success: function (card) {
                 let hit = (playerOldValue + card.name)
-                // if (hit == 21){
-                //     let 
-                //     $("#hit").html(card);
-                // }
                 $("#hit").html(hit);
             }
         });
 
 
     });
-});
 
+    $("#stand").click(function () {
+        let count = 1
+
+        if ($('#tbCard1').text() == '?') {
+            $.get('/gethit', e => {
+                $("#tbCard1").html(e.name)
+            })
+
+        } else {
+            let tbOldValue = $('#game').text()
+            $.get('/gethit', e => { $("#game").html(tbOldValue + e.name) })
+        }
+        let joker = $('#tbCard1').text()
+
+
+        $.get('/getCardValue', { 'value': $('#game').text().slice(-2, -1) }).done(e => {
+            $.get('/getCardValue', { 'value': $('#game').text().slice(-2, -1) }).done(e => {
+
+                let scndLastValue = e
+                $.get('/getCardValue', { 'value': $('#game').text().slice(-1) }).done(e => {
+                    let tbScore = $("#tbScore").text()
+
+                    let value;
+
+                    if (joker == '?') {
+                        let value = (parseInt(scndLastValue) + parseInt(e))
+                        $("#tbScore").html(value)
+                    } else {
+                        let value = (parseInt(tbScore) + parseInt(e))
+                        $("#tbScore").html(value)
+                    }
+
+                    setTimeout(function(){
+
+                    if ($("#tbScore").text() <= 17 && $("#tbScore").text() !== '') {
+                        console.log($("#tbScore").text())
+                        $("#stand").trigger("click");
+                    } else {
+
+                        if ($("#tbScore").text() > 21) {
+                            $("#tbStatus").html('TABLE LOSE!')
+                            $("#plStatus").html('YOU WIN!')
+                            $("#stand").prop("disabled", true);
+                        } else if ($("#tbScore").text() == 21) {
+                            $("#tbStatus").html('TABLE WIN!')
+                            $("#plStatus").html('YOU LOSE!')
+                            $("#stand").prop("disabled", true);
+                        } else if ($("#tbScore").text() < 21 && ($("#tbScore").text() > $("#plScore").text())) {
+                            $("#tbStatus").html('TABLE WIN!')
+                            $("#plStatus").html('YOU LOSE!')
+                            $("#stand").prop("disabled", true);
+                        }  else if ($("#tbScore").text() < 21 && ($("#tbScore").text() < $("#plScore").text())) {
+                            $("#tbStatus").html('TABLE LOSE!')
+                            $("#plStatus").html('YOU WIN!')
+                            $("#stand").prop("disabled", true);
+                        }
+                    }
+                    }, 800);
+
+                })
+
+            })
+
+        })
+
+    })
+    console.log($('#tbCard1').text())
+
+
+
+})
